6ff1f65c17670e8c959205ab01f3ac649240c2d0
Brieuuc <118290382+Brieuuc@users.noreply.github.com>
2022-11-15 19:42:54 +0100

    .html to .php / Added : php include header.html

---
+++ html/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Aux bonnes recettes</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="icon" type="image/x-icon" href="../images/logo.png">
+</head>
+<body>
+	<?php include 'header.html' ?>
+</body>
+</html>
---
+++ html/search.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Aux bonnes recettes</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="icon" type="image/x-icon" href="../images/logo.png">
+</head>
+<body>
+	<?php include 'header.html'?>
+<section>
+	
+</section>
+</body>
+</html>
