477d419f2312be06a6f54f9d3a32701f7dcda523
Brieuuc <brieuc.huot@gmail.com>
2022-12-14 16:04:14 +0100

    Create Recipe.php / Affichage Recette,Avis et comments form

---
+++ html/recipe.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Aux bonnes recettes</title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="icon" type="image/x-icon" href="/images/logo.png">
+</head>
+<body>
+<!-- En-tête de la page -->
+	<?php include 'header.php' ?>
+<!-- PHP Ajout Commentaire -->
+    <?php
+    if (!empty($_POST)){
+        if (hasAlreadyRated($_POST['id'], $_SERVER['REMOTE_ADDR'])){
+            echo "Erreur : Vous avez déjà émis un avis sur cette recette !";
+        }
+        else{
+        rateRecipe($_POST['id'], $_SERVER['REMOTE_ADDR'], $_POST['note'], $_POST['comment']);
+        echo "Votre avis a été publié !";
+        }
+    }
+    ?>
+<!-- PHP Récupération détails recette -->
+    <?php $Recipe = getRecipe($_GET['id']); ?>
+<!-- Affichage Recette -->
+    <div class="recette">
+        <h2><?php echo $Recipe['title'];?></h2>
+        <!-- Affichage Note / Temps de préparation SI AUCUN AVIS-->
+        <?php
+        if (empty(getRecipeComments($_GET['id']))){
+            echo "<div>Note des internautes : Aucun (0 avis)  -  <img src='../images/hourglass.png' class='logo_manage' alt='Temps'>".$Recipe['time']." minutes</div>";
+        }
+        // Affichage Note / Temps de préparation SI AVIS EXISTANT(S)
+        else{
+            $RecipeComments = getRecipeComments($_GET['id']);
+            $Notes = 0;
+            foreach($RecipeComments as $Note){
+                $Notes = $Notes + $Note['note'];
+            }
+            $Notes = $Notes / count($RecipeComments);
+            echo "<div>Note des internautes : ".$Notes." (".count($RecipeComments)." avis)  -  <img src='../images/hourglass.png' class='logo_manage' alt='Temps'>".$Recipe['time']." minutes</div>";
+        }
+        ?>
+        <!-- Affichage Ingrédients / Consignes de préparation -->
+        <h3>Ingrédients</h3>
+        <p><?php echo $Recipe['ingredients'];?></p>
+        <h3>Etapes</h3>
+        <p><?php echo $Recipe['steps'];?></p>
+    </div>
+<!-- Section Commentaire -->
+    <hr><p>SECTION COMMENTAIRE</p>
+    <!-- Formulaire Commentaire -->
+    <div class="comments_form">
+        <p>Mettre un commentaire</p>
+        <form method="POST">
+            <input name="comment" type="text" placeholder="Entrez votre commentaire" required="required">
+            <select name="note">
+                <option value="">Choisissez une note</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <button type="submit" target="recipe.php?id=<?php echo $_GET['id']?>">Mettre l'avis</button>
+            <input name="id" type="hidden" value="<?php echo $_GET['id']?>" required="required"><br>
+        </form>
+    </div>
+    <hr>
+    <!-- Affichage Commentaire(s) -->
+    <div class="comments_list">
+    <?php
+        if (empty(getRecipeComments($_GET['id']))){
+            echo "Aucun avis sur cette recette. Soyez le premier à en mettre un via le formulaire ci-dessus !";
+        }
+        else{
+            foreach ($RecipeComments as $Comment){
+                echo "<div class='comment'>Note : ".$Comment['note']."<br>Commentaire : ".$Comment['comment']."</div>";
+            }
+        }
+    ?>
+    </div>
+	<?php include 'footer.html' ?>
+</body>
+</html>
