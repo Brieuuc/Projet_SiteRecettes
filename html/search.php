102edb9ed5ed540ff0ba41a99779c45bd6767d1d
Brieuuc <brieuc.huot@gmail.com>
2023-01-08 12:58:35 +0100

    Search.php terminé

--- html/search.php
+++ html/search.php
@@ -7,8 +7,13 @@
 	<link rel="icon" type="image/x-icon" href="/images/logo.png">
 </head>
 <body>
-	<?php include 'header.php'?>
-	<?php
+	<?php include 'header.php';
+	// Si on est sur la page et qu'il n'y aucune de données de recherche envoyées
+	if (empty($_POST['search'])){
+		echo "<p>Veuillez indiquer une recette ou un ingrédient dans la recherche ci-dessus.</p>";
+	}
+	// Sinon si on a des données de recherche
+	else{
 		// Récupère les résultats correspondants aux deux types de recherche et les fusionnent
 		$searchTitle = searchRecipesByTitle($_POST['search']);
 		$searchIngredients = searchRecipesByIngredient($_POST['search']);
@@ -29,21 +34,36 @@
 		foreach($doublonList as $doublon){
 			unset($searchResults[$doublon]);
 		}
-	?>
-	<p>Page recherche</p>
-
-	<?php if (empty($_POST['search'])){
-		echo "<p>Veuillez indiquer une recette dans la recherche ci-dessus.</p>";
-	}
-	else{
+		// Si aucun résultat
 		if (empty($searchResults)){
+			
 			echo "<p>Aucun résultat trouvé !";
 		}
+		// Affichage des résultats
 		else{
-			echo "<p>Résultats trouvés : ".count($searchResults)."</p>";
-
+			echo "<div class=result_list>";
+			echo "<p class=search_title>Recherche : ".$_POST['search']." - Nombre de résultat : ".count($searchResults)."</p>";
+			$pas = 0;
+			foreach($searchResults as $Recipe){
+				$Note = moyenneNote($Recipe);
+				if ($Note == -1){
+					$Note = 'Aucun Avis';
+				}
+				else{
+					$Note = $Note.'/5';
+				}
+				echo "
+				<div class='search_recette_".($pas%2)."'>
+					<h3>".$Recipe['title']."</h3>
+					<p>Note : ".$Note." - Temps de préparation : ".$Recipe['time']." minutes</p>
+					<a href='recipe?id=".$Recipe['id']."'>Voir la recette</a>
+				</div>";
+				$pas++;
+			}
+			echo "</div>";
 		}
-	} ?>
+	}
+	?>
 	<?php include 'footer.html' ?>
 </body>
 </html>
